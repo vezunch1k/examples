b748ea66b962a6025f1a58df006d2d4127b786e8
vezunch1k <vezunch1k@users.noreply.github.com>
2014-09-01 22:39:41 +0400

    Update graph_editor.py
    Added edge options, its setting.

--- graph_editor.py
+++ graph_editor.py
@@ -12,6 +12,29 @@ class Vertex(object):
         self.options = {}
 
     def set_option(self, key, value):
+        if re.search('[^a-zA-Z_0-9]', key):
+            raise AttributeError('You can use only letters, digits and _')
+        self.options[key] = value
+
+    def remove_option(self, key):
+        if key not in self.options:
+            raise AttributeError('No option with key %s' % key)
+        del self.options[key]
+
+
+class Edge(object):
+    def __init__(self, name1, name2):
+        if re.search('[^a-zA-Z_0-9]', name1):
+            raise AttributeError('You can use only letters, digits and _')
+        if re.search('[^a-zA-Z_0-9]', name2):
+            raise AttributeError('You can use only letters, digits and _')
+        self.name1 = name1
+        self.name2 = name2
+        self.options = {}
+
+    def set_option(self, key, value):
+        if re.search('[^a-zA-Z_0-9]', key):
+            raise AttributeError('You can use only letters, digits and _')
         self.options[key] = value
 
     def remove_option(self, key):
@@ -46,25 +69,26 @@ class Graph(object):
             raise AttributeError('No vertex with name %s' % name1)
         elif not self.has_vertex(name2):
             raise AttributeError('No vertex with name %s' % name2)
-        elif self.has_edge(name2, name1) or self.has_edge(name1, name2):
-            raise AttributeError('Graph already has such edge')
         else:
-            self.edge_list[''.join((name1, ':', name2))] = True
+            (name1, name2) = sorted([name1, name2])
+            self.edge_list[''.join((name1, ':', name2))] = Edge(name1, name2)
 
     def remove_edge(self, name1, name2):
-        if not self.has_edge(name1, name2) and not self.has_edge(name2, name1):
+        (name1, name2) = sorted([name1, name2])
+        if not self.has_edge(name1, name2):
             raise AttributeError('No edge between %s and %s' % (name1, name2))
-        else:
-            if self.has_edge(name1, name2):
-                edge = ''.join((name1, ':', name2))
-            else:
-                edge = ''.join((name2, ':', name1))
-            del self.edge_list[edge]
+        del self.edge_list[''.join((name1, ':', name2))]
 
     def get_vertex(self, name):
         if not self.has_vertex(name):
             raise AttributeError('No vertex in graph with name %s' % name)
         return self.vertex_list[name]
+
+    def get_edge(self, name1, name2):
+        (name1, name2) = sorted([name1, name2])
+        if not self.has_edge(name1, name2):
+            raise AttributeError('No edge between %s and %s' % (name1, name2))
+        return self.edge_list[''.join((name1, ':', name2))]
 
 
 class Activity(object):
@@ -115,12 +139,10 @@ class Document(object):
     def set_vertex_attribute(self, obj):
         vertex = mygraph.get_vertex(obj.name)
         vertex.set_option(obj.key, obj.value)
-        mygraph.add_vertex(vertex)
 
     def remove_vertex_attribute(self, obj):
         vertex = mygraph.get_vertex(obj.name)
         vertex.remove_option(obj.key)
-        mygraph.add_vertex(vertex)
 
     def print_graph(self, *args):
         buffer = ['graph mygraph {\n']
@@ -134,8 +156,15 @@ class Document(object):
                 options_str = ''.join([' [', options_buffer, ']'])
             buffer.extend([k, options_str, ';\n'])
         for k, v in mygraph.edge_list.iteritems():
+            options_str = ''
+            if v.options:
+                options_buffer = []
+                for o, m in v.options.iteritems():
+                    options_buffer.extend([''.join([o, '=', m])])
+                options_buffer = ' '.join(options_buffer)
+                options_str = ''.join([' [', options_buffer, ']'])
             (name1, name2) = k.split(':')
-            buffer.extend([name1, ' -- ', name2, ';\n'])
+            buffer.extend([name1, ' -- ', name2, options_str, ';\n'])
         buffer.append('}\n')
         sys.stdout.write(''.join(buffer))
 
@@ -144,6 +173,14 @@ class Document(object):
 
     def remove_edge(self, obj):
         mygraph.remove_edge(obj.name1, obj.name2)
+
+    def set_edge_attribute(self, obj):
+        edge = mygraph.get_edge(obj.name1, obj.name2)
+        edge.set_option(obj.key, obj.value)
+
+    def remove_edge_attribute(self, obj):
+        edge = mygraph.get_edge(obj.name1, obj.name2)
+        edge.remove_option(obj.key)
 
     def undo(self, *args):
         myhistory.do_undo()
@@ -164,11 +201,12 @@ def get_parser():
     p_rv.add_argument('name')
     p_rv.set_defaults(func=mydoc.remove_vertex, antifunc=mydoc.add_vertex)
 
-    p_sv = subparsers.add_parser('set_vertex_attribute', help='set attribute')
-    p_sv.add_argument('name')
-    p_sv.add_argument('key')
-    p_sv.add_argument('value')
-    p_sv.set_defaults(
+    p_sva = subparsers.add_parser(
+        'set_vertex_attribute', help='setting vertex attribute')
+    p_sva.add_argument('name')
+    p_sva.add_argument('key')
+    p_sva.add_argument('value')
+    p_sva.set_defaults(
         func=mydoc.set_vertex_attribute,
         antifunc=mydoc.remove_vertex_attribute
     )
@@ -183,17 +221,28 @@ def get_parser():
     p_re.add_argument('name2')
     p_re.set_defaults(func=mydoc.remove_edge, antifunc=mydoc.add_edge)
 
-    p_pg = subparsers.add_parser('print', help='graph printing')
-    p_pg.set_defaults(func=mydoc.print_graph, antifunc=None)
-
-    p_exit = subparsers.add_parser('exit', help='exits the editor')
-    p_exit.set_defaults(func=mydoc.exit, antifunc=None)
+    p_sea = subparsers.add_parser(
+        'set_edge_attribute', help='setting edge attribute')
+    p_sea.add_argument('name1')
+    p_sea.add_argument('name2')
+    p_sea.add_argument('key')
+    p_sea.add_argument('value')
+    p_sea.set_defaults(
+        func=mydoc.set_edge_attribute,
+        antifunc=mydoc.remove_edge_attribute
+    )
 
     p_undo = subparsers.add_parser('undo', help='undo the last action')
     p_undo.set_defaults(func=mydoc.undo, antifunc=None)
 
     p_redo = subparsers.add_parser('redo', help='redo the last action')
     p_redo.set_defaults(func=mydoc.redo, antifunc=None)
+
+    p_pg = subparsers.add_parser('print', help='graph printing')
+    p_pg.set_defaults(func=mydoc.print_graph, antifunc=None)
+
+    p_exit = subparsers.add_parser('exit', help='exits the editor')
+    p_exit.set_defaults(func=mydoc.exit, antifunc=None)
     return parser
 
 if __name__ == '__main__':
